21fc7dacdea73a032fa9717aa0b12c4c3f2ba733
Aaditya Mohan <aadityamohan@Aadityas-MacBook-Air.local>
2025-01-01 12:38:32 +0530

    minor fix

--- src/main/java/com/aadi/bank/the_first_bank/controller/TransactionsController.java
+++ src/main/java/com/aadi/bank/the_first_bank/controller/TransactionsController.java
@@ -2,6 +2,7 @@ package com.aadi.bank.the_first_bank.controller;
 
 import com.aadi.bank.the_first_bank.entity.Transaction;
 import com.aadi.bank.the_first_bank.services.BankStatement;
+import com.itextpdf.text.DocumentException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,7 @@ import java.util.List;
 public class TransactionsController {
     private BankStatement bankStatement;
     @GetMapping
-    public List<Transaction>  generateBankStatement(@RequestParam String accountNumber,@RequestParam String startDate, @RequestParam String endDate){
+    public List<Transaction>  generateBankStatement(@RequestParam String accountNumber,@RequestParam String startDate, @RequestParam String endDate) throws DocumentException {
             return bankStatement.generateStatement(accountNumber,startDate,endDate);
     }
 }
